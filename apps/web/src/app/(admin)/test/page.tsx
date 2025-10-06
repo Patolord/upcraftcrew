cf1cf2bf1a0e4b29d1a95348a5fbfdf762ceff8c
Paloma <paloma.sq@hotmail.com>
2025-10-05 21:39:49 -0300

    limpeza do menu do perfil

---
+++ apps/web/src/app/(admin)/test/page.tsx
@@ -0,0 +1,9 @@
+
+import { UsageStatsMenuDemo } from "@/app/components/layouts/profile-menu/UsageStatsMenuDemo";
+
+export default function TestPage() {
+    
+	return <div className="flex h-full w-full items-center justify-center">
+        <UsageStatsMenuDemo/>
+    </div>;
+}
