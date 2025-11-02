075007688c196f65162663020cc38b8f07d6eef0
Paloma <paloma.sq@hotmail.com>
2025-11-02 13:11:10 -0300

    Atualizacao (#31)
    * Atualização: salvamento de alterações locais
    
    * Update ProfileMenu component to indicate sign-out action as a TODO item
    
    * Aplicar mudanças da master: atualizações em auth.server.ts
    
    * Update TypeScript configuration to use "react-jsx" for JSX transformation in tsconfig.json
    
    * Update ProfileMenu component to remove TODO indication from Sign Out action
    
    ---------
    
    Co-authored-by: Rodrigo Luiz Schil Pellegrini <rodrigo_luizp@hotmail.com>

--- apps/web/src/lib/auth.server.ts
+++ apps/web/src/lib/auth.server.ts
@@ -1,6 +1,8 @@
 import { createAuth } from "@upcraftcrew-os/backend/convex/auth";
 import { getToken as getTokenNextjs } from "@convex-dev/better-auth/nextjs";
+import type { GenericDataModel } from "convex/server";
+import type { CreateAuth } from "@convex-dev/better-auth";
 
 export const getToken = () => {
-	return getTokenNextjs(createAuth);
+	return getTokenNextjs(createAuth as CreateAuth<GenericDataModel>);
 };
