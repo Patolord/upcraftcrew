b2c565dfcb1d2487e86e947608eb344577744ea7
Paloma <134529281+sqpaloma@users.noreply.github.com>
2025-11-16 14:45:49 -0300

    Enhance app configuration and update dependencies
    - Added new dependencies for Better Auth and Convex integration in package.json.
    - Updated app.json to include versionCode for Android.
    - Modified eas.json to set release status and track for production builds.
    - Updated pnpm-lock.yaml to include new package versions and configurations.
    - Refactored _layout.tsx and auth-client.ts to improve authentication handling and environment variable management.
    
    These changes aim to improve the app's configuration, enhance authentication flow, and ensure compatibility with the latest dependencies.

--- apps/native/app/_layout.tsx
+++ apps/native/app/_layout.tsx
@@ -1,20 +1,19 @@
 import "@/global.css";
 
-import { ConvexReactClient } from "convex/react";
 import { ConvexBetterAuthProvider } from "@convex-dev/better-auth/react";
-import { authClient } from "@/lib/auth-client";
-
+import { ConvexReactClient } from "convex/react";
 import { Stack } from "expo-router";
 import { HeroUINativeProvider } from "heroui-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { KeyboardProvider } from "react-native-keyboard-controller";
 import { AppThemeProvider } from "@/contexts/app-theme-context";
+import { authClient } from "@/lib/auth-client";
 
 export const unstable_settings = {
 	initialRouteName: "(drawer)",
 };
-
-const convexUrl = process.env.EXPO_PUBLIC_CONVEX_URL || "";
+// TODO: change to process.env.EXPO_PUBLIC_CONVEX_URL
+const convexUrl = "https://curious-rhinoceros-373.convex.cloud";
 const convex = new ConvexReactClient(convexUrl, {
 	unsavedChangesWarning: false,
 });
--- apps/native/lib/auth-client.ts
+++ apps/native/lib/auth-client.ts
@@ -1,12 +1,13 @@
-import { createAuthClient } from "better-auth/react";
-import { anonymousClient } from "better-auth/client/plugins";
-import { convexClient } from "@convex-dev/better-auth/client/plugins";
 import { expoClient } from "@better-auth/expo/client";
+import { convexClient } from "@convex-dev/better-auth/client/plugins";
+import { anonymousClient } from "better-auth/client/plugins";
+import { createAuthClient } from "better-auth/react";
 import Constants from "expo-constants";
 import * as SecureStore from "expo-secure-store";
 
 export const authClient = createAuthClient({
-	baseURL: process.env.EXPO_PUBLIC_CONVEX_SITE_URL,
+	// TODO: change to process.env.EXPO_PUBLIC_CONVEX_SITE_URL
+	baseURL: "https://curious-rhinoceros-373.convex.site",
 	plugins: [
 		anonymousClient(),
 		expoClient({
